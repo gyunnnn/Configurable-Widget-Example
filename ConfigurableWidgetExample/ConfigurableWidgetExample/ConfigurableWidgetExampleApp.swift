6da78a312d70ddb7d6e4a0c8bd69f60f9bd5b547
codebokkeum <jegyun@icloud.com>
2026-01-29 13:15:01 +0900

    🎉 Init: Project init

---
+++ ConfigurableWidgetExample/ConfigurableWidgetExample/ConfigurableWidgetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfigurableWidgetExampleApp.swift
+//  ConfigurableWidgetExample
+//
+//  Created by 박제균 on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ConfigurableWidgetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConfigurableWidgetExample/ConfigurableWidgetExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConfigurableWidgetExample
+//
+//  Created by 박제균 on 1/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
