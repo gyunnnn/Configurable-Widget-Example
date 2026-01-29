8a60730be799cc6e863b18e6fd72eb389f4c8c05
codebokkeum <jegyun@icloud.com>
2026-01-29 13:35:18 +0900

    🔧 Add: Widget Extension

---
+++ ConfigurableWidgetExample/ExampleWidget/ExampleWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ExampleWidgetBundle.swift
+//  ExampleWidget
+//
+//  Created by 박제균 on 1/29/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ExampleWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ExampleWidget()
+    }
+}
